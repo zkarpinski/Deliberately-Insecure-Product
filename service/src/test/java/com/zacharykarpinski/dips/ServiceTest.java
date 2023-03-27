109d94aaa74d94d111b089d9896df62df8881fa2
Zachary Karpinski <1206496+zkarpinski@users.noreply.github.com>
2023-03-27 15:18:02 -0400

    added micronaut

---
+++ service/src/test/java/com/zacharykarpinski/dips/ServiceTest.java
@@ -0,0 +1,21 @@
+package com.zacharykarpinski.dips;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import javax.inject.Inject;
+
+@MicronautTest
+class ServiceTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
